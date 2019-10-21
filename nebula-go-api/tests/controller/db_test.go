e2b2e59363142741f0363dba3b50167a88a8dfa0
NicolaCage <37568394+NicolaCage@users.noreply.github.com>
2019-10-21 09:31:58 +0800

    feat: add api test

--- nebula-go-api/tests/controller/db_test.go
+++ nebula-go-api/tests/controller/db_test.go
@@ -1,8 +1,87 @@
-// package tests
+package tests
 
-// import (
-// 	"encoding/json"
-// 	beetest "github.com/astaxie/beego/testing"
-// 	"testing"
-// )
+import (
+	"testing"
+	"net/http"
+	"bytes"
+	"io/ioutil"
+	"fmt"
+)
 
+func Test_DB_Connect(t *testing.T) {
+
+	cases := []struct {
+		path          string
+		requestMethod string
+		requestBody   []byte
+	}{
+		{	
+			"http://127.0.0.1:8080/api/db/connect",
+			"POST",
+			[]byte(`{"username": "user",
+					"password": "password",
+					"host": "127.0.0.1:3699"}`),
+		},
+		{	
+			"http://127.0.0.1:8080/api/db/connect",
+			"POST",
+			[]byte(`{"username": "user1",
+					"password": "password",
+					"host": "127.0.0.1:3699"}`),
+		},
+	}
+	for _, tc := range cases {
+
+		req, err := http.NewRequest(tc.requestMethod, tc.path, bytes.NewBuffer(tc.requestBody))
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		fmt.Println("client :", client)
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+
+		defer req.Body.Close()
+
+ 		body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
+	}
+}
+
+
+func Test_DB_Execute(t *testing.T) {
+
+	cases := []struct {
+		path          string
+		requestMethod string
+		requestBody   []byte
+	}{
+		{	
+			"http://127.0.0.1:8080/api/db/exec",
+			"POST",
+			[]byte(`{"username" : "user",
+					"password" : "password",
+					"host" : "127.0.0.1:3699",
+					"gql" : "SHOW SPACES;"}`),
+		},
+	}
+	for _, tc := range cases {
+		
+		req, err := http.NewRequest(tc.requestMethod,tc.path, bytes.NewBuffer(tc.requestBody))
+		req.Header.Set("Content-Type", "application/json")
+		
+		client := &http.Client{}
+		fmt.Println("client :", client)
+		resp, err := client.Do(req)
+		
+    if err != nil {
+        panic(err)
+		}
+		
+		defer resp.Body.Close()
+		
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
+	}
+}
