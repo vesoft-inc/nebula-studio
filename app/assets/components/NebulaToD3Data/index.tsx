324f3796355e2c90e59bbf427cbe6a50071a64bc
NicolaCage <37568394+NicolaCage@users.noreply.github.com>
2019-11-07 18:53:05 +0800

    feat: d3 add select region node

--- app/assets/components/NebulaToD3Data/index.tsx
+++ app/assets/components/NebulaToD3Data/index.tsx
@@ -177,6 +177,51 @@ class NebulaToD3Data extends React.Component<IProps, {}> {
           return (d.source.y + d.target.y) / 2;
         });
     });
+
+    if (data.nodes.length !== 0) {
+      const startPoint = {
+        x: 0,
+        y: 0,
+      };
+      const rect = svg
+        .append('rect')
+        .style('stroke', 'gray')
+        .style('stroke-width', '0.6')
+        .style('fill', 'transparent')
+        .style('stroke-opacity', '0.6');
+      d3.selectAll('svg')
+        .on('mousedown', () => {
+          startPoint.x = d3.event.offsetX;
+          startPoint.y = d3.event.offsetY;
+        })
+        .on('mousemove', () => {
+          if (startPoint.x !== 0) {
+            rect
+              .attr('x', Math.min(d3.event.offsetX, startPoint.x))
+              .attr('y', Math.min(d3.event.offsetY, startPoint.y))
+              .attr('width', Math.abs(d3.event.offsetX - startPoint.x))
+              .attr('height', Math.abs(d3.event.offsetY - startPoint.y));
+          }
+        })
+        .on('mouseup', () => {
+          const nodes = data.nodes;
+          for (let _i: number = 0; _i < nodes.length; _i++) {
+            const nodePoint = data.nodes[_i];
+            if (
+              (nodePoint.x > startPoint.x && nodePoint.x > d3.event.offsetX) ||
+              (nodePoint.x < startPoint.x && nodePoint.x < d3.event.offsetX) ||
+              (nodePoint.y > startPoint.y && nodePoint.y > d3.event.offsetY) ||
+              (nodePoint.y < startPoint.y && nodePoint.y < d3.event.offsetY)
+            ) {
+              continue;
+            }
+            console.log(nodePoint);
+          }
+          startPoint.x = 0;
+          startPoint.y = 0;
+          rect.attr('width', 0).attr('height', 0);
+        });
+    }
   }
 
   render() {
