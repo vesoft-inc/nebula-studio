d2911b05eb8bef1e2b6f50d610516f8d054a6cd7
NicolaCage <37568394+NicolaCage@users.noreply.github.com>
2019-11-11 15:48:59 +0800

    mod: change d3 rect

--- app/assets/components/NebulaToD3Data/index.tsx
+++ app/assets/components/NebulaToD3Data/index.tsx
@@ -5,6 +5,7 @@ import './index.less';
 import Labels from './labels';
 import Links from './links';
 import Nodes from './nodes';
+import Rect from './rect';
 
 interface INode extends d3.SimulationNodeDatum {
   name: string;
@@ -124,59 +125,6 @@ class NebulaToD3Data extends React.Component<IProps, {}> {
           return (d.source.y + d.target.y) / 2;
         });
     });
-
-    if (data.vertexs.length !== 0) {
-      const startPoint = {
-        x: 0,
-        y: 0,
-      };
-      const rect = svg
-        .append('rect')
-        .style('stroke', 'gray')
-        .style('stroke-width', '0.6')
-        .style('fill', 'transparent')
-        .style('stroke-opacity', '0.6');
-      d3.selectAll('svg')
-        .on('mousedown', () => {
-          startPoint.x = d3.event.offsetX;
-          startPoint.y = d3.event.offsetY;
-        })
-        .on('mousemove', () => {
-          if (startPoint.x !== 0) {
-            rect
-              .attr('x', Math.min(d3.event.offsetX, startPoint.x))
-              .attr('y', Math.min(d3.event.offsetY, startPoint.y))
-              .attr('width', Math.abs(d3.event.offsetX - startPoint.x))
-              .attr('height', Math.abs(d3.event.offsetY - startPoint.y));
-          }
-        })
-        .on('mouseup', () => {
-          const nodes = data.vertexs;
-          const len = nodes.length;
-          for (let _i: number = 0; _i < len; _i++) {
-            const nodePoint: any = nodes[_i];
-            if (this.isNotSelected(nodePoint, startPoint)) {
-              continue;
-            }
-            console.log(nodePoint);
-          }
-          startPoint.x = 0;
-          startPoint.y = 0;
-          rect.attr('width', 0).attr('height', 0);
-        });
-    }
-  }
-
-  isNotSelected(nodePoint, startPoint) {
-    if (
-      (nodePoint.x > startPoint.x && nodePoint.x > d3.event.offsetX) ||
-      (nodePoint.x < startPoint.x && nodePoint.x < d3.event.offsetX) ||
-      (nodePoint.y > startPoint.y && nodePoint.y > d3.event.offsetY) ||
-      (nodePoint.y < startPoint.y && nodePoint.y < d3.event.offsetY)
-    ) {
-      return true;
-    }
-    return false;
   }
 
   dragged(d) {
@@ -212,6 +160,7 @@ class NebulaToD3Data extends React.Component<IProps, {}> {
         <Links links={data.edges} />
         <Nodes nodes={data.vertexs} />
         <Labels nodes={data.vertexs} />
+        <Rect nodes={data.vertexs} />
       </svg>
     );
   }
