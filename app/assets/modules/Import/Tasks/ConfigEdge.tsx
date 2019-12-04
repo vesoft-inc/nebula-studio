68621999e8f5e9500c418c98998050319672449a
Jerry <8124783+nianiaJR@users.noreply.github.com>
2019-12-04 19:51:14 +0800

    Jerry dev (#44)
    * feat: delete tag
    
    * feat: add edge config logic
    
    * refactor: move deleteTagconfig to reducer && fix bugs
    
    * refactor: rename node to vertex

--- app/assets/modules/Import/Tasks/ConfigEdge.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class ConfigEdge extends React.Component {
-  render() {
-    return <div className="init">config edge</div>;
-  }
-}
---
+++ app/assets/modules/Import/Tasks/ConfigEdge/index.tsx
@@ -0,0 +1,97 @@
+import { Button, Icon, Tabs } from 'antd';
+import React from 'react';
+import intl from 'react-intl-universal';
+import { connect } from 'react-redux';
+
+import CSVPreviewLink from '#assets/components/CSVPreviewLink';
+import { IDispatch, IRootState } from '#assets/store';
+
+import Add, { AddType } from '../Add';
+import Edge from './Edge';
+import './index.less';
+
+const { TabPane } = Tabs;
+
+const mapState = (state: IRootState) => ({
+  edgesConfig: state.importData.edgesConfig,
+  activeEdgeIndex: state.importData.activeEdgeIndex,
+});
+
+const mapDispatch = (dispatch: IDispatch) => ({
+  updateActiveEdgeIndex: edgeIndex => {
+    dispatch.importData.update({
+      activeEdgeIndex: edgeIndex,
+    });
+  },
+  deleteEdgeConfig: edgeName => {
+    dispatch.importData.deleteEdgeConfig({ edgeName });
+  },
+});
+
+interface IProps
+  extends ReturnType<typeof mapState>,
+    ReturnType<typeof mapDispatch> {}
+
+class ConfigEdge extends React.Component<IProps> {
+  handleTabClick = key => {
+    const index = Number(key.split('#')[1]);
+    this.props.updateActiveEdgeIndex(index);
+  };
+
+  render() {
+    const { edgesConfig, activeEdgeIndex } = this.props;
+
+    return (
+      <div className="edge-config-task task">
+        <div className="edges">
+          <h3>Edge Types</h3>
+          <div className="operation">
+            <Add type={AddType.edge} />
+          </div>
+          <Tabs
+            className="edge-tabs"
+            activeKey={`${edgesConfig[activeEdgeIndex] &&
+              edgesConfig[activeEdgeIndex].name}#${activeEdgeIndex}`}
+            onTabClick={this.handleTabClick}
+          >
+            {edgesConfig.map((edge, index) => (
+              <TabPane
+                tab={
+                  <p className="tab-content">
+                    {edge.name}
+                    <Button
+                      type="link"
+                      onClick={e => {
+                        e.stopPropagation();
+                        this.props.deleteEdgeConfig(edge.name);
+                      }}
+                    >
+                      <Icon type="close" />
+                    </Button>
+                  </p>
+                }
+                key={`${edge.name}#${index}`}
+              >
+                <span className="csv-file">
+                  File:
+                  <CSVPreviewLink file={edge.file}>
+                    {edge.file.name}
+                  </CSVPreviewLink>
+                  <Edge />
+                </span>
+              </TabPane>
+            ))}
+          </Tabs>
+        </div>
+        <Button className="next" type="primary">
+          {intl.get('import.next')}
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  mapState,
+  mapDispatch,
+)(ConfigEdge);
