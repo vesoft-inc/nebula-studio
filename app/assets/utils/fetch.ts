14081eac1c64e912360c2ebc3f3627e071f41aab
NicolaCage <37568394+NicolaCage@users.noreply.github.com>
2020-01-09 17:21:56 +0800

    mod: change type

--- app/assets/utils/fetch.ts
+++ app/assets/utils/fetch.ts
@@ -2,7 +2,7 @@ import service from '#assets/config/service';
 
 export async function fetchVertexId(
   { space, host, username, password }: any,
-  id: any[],
+  id: any,
 ) {
   const gql = `use ${space}; fetch prop on * ${id}`;
   const { data } = (await service.execNGQL({
