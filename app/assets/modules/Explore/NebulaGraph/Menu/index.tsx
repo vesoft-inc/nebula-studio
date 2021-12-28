081cdc79d73c6caeebccc0708c3fea0852fec545
Nut He <18328704+hetao92@users.noreply.github.com>
2021-12-28 18:54:03 +0800

    fix: fix menu height in explore (#66)

--- app/assets/modules/Explore/NebulaGraph/Menu/index.tsx
+++ app/assets/modules/Explore/NebulaGraph/Menu/index.tsx
@@ -17,6 +17,44 @@ interface IProps {
   width: number;
   height: number;
 }
+const menuConfig = [
+  {
+    component: <ExpandBtn showTitle={true} />,
+  },
+  {
+    component: <UnExpandBtn showTitle={true} />,
+  },
+  {
+    component: <ZoomBtn type="zoom-in" showTitle={true} />,
+  },
+  {
+    component: <ZoomBtn type="zoom-out" showTitle={true} />,
+  },
+  {
+    component: <VertexStyleSetBtn showTitle={true} />,
+  },
+  {
+    component: <PropsDisplayBtn showTitle={true} />,
+  },
+  {
+    component: <Unlock showTitle={true} />,
+  },
+  {
+    component: <Lock showTitle={true} />,
+  },
+  {
+    component: <RollbackBtn showTitle={true} />,
+  },
+  {
+    component: <DeleteBtn showTitle={true} />,
+  },
+  {
+    component: <SearchBtn showTitle={true} />,
+  },
+];
+
+const DEFAULT_MENU_HEIGHT = menuConfig.length * 43;
+
 class Menu extends React.PureComponent<IProps> {
   componentDidMount() {
     const self = this;
@@ -26,11 +64,15 @@ class Menu extends React.PureComponent<IProps> {
         d3.event.preventDefault();
       })
       .on('click', () => {
-        d3.select('.d3-click-right-menu').style('visibility', 'hidden');
+        d3.select('.d3-click-right-menu')
+          .style('visibility', 'hidden')
+          .style('height', `${DEFAULT_MENU_HEIGHT}px`);
       });
 
     d3.selectAll('.panel-btn-item').on('click', () => {
-      d3.select('.d3-click-right-menu').style('visibility', 'hidden');
+      d3.select('.d3-click-right-menu')
+        .style('visibility', 'hidden')
+        .style('height', `${DEFAULT_MENU_HEIGHT}px`);
     });
   }
 
@@ -50,51 +92,34 @@ class Menu extends React.PureComponent<IProps> {
         left = left - menuWidth;
       }
       if (menuHeight + top > height) {
-        top = top - menuHeight;
+        if (height - top > top) {
+          $rightMenu
+            .style('height', `${height - top - 30}px`)
+            .style('overflow', 'auto');
+        } else {
+          top =
+            top - menuHeight < 0
+              ? height - event.offsetY - 10
+              : top - menuHeight;
+          let _menuHeight = DEFAULT_MENU_HEIGHT;
+          if (top < 0) {
+            _menuHeight = height - event.offsetY - 10;
+          } else if (_menuHeight + top > height) {
+            _menuHeight = height - top - 30;
+          }
+          $rightMenu
+            .style('height', `${_menuHeight}px`)
+            .style('overflow', 'auto');
+        }
       }
       $rightMenu
-        .style('left', left + 'px')
-        .style('top', top + 'px')
+        .style('left', `${left}px`)
+        .style('top', `${top}px`)
         .style('visibility', 'visible');
     }
   };
 
   render() {
-    const menuConfig = [
-      {
-        component: <ExpandBtn showTitle={true} />,
-      },
-      {
-        component: <UnExpandBtn showTitle={true} />,
-      },
-      {
-        component: <ZoomBtn type="zoom-in" showTitle={true} />,
-      },
-      {
-        component: <ZoomBtn type="zoom-out" showTitle={true} />,
-      },
-      {
-        component: <VertexStyleSetBtn showTitle={true} />,
-      },
-      {
-        component: <PropsDisplayBtn showTitle={true} />,
-      },
-      {
-        component: <Unlock showTitle={true} />,
-      },
-      {
-        component: <Lock showTitle={true} />,
-      },
-      {
-        component: <RollbackBtn showTitle={true} />,
-      },
-      {
-        component: <DeleteBtn showTitle={true} />,
-      },
-      {
-        component: <SearchBtn showTitle={true} />,
-      },
-    ];
     return (
       <div className="d3-click-right-menu">
         {menuConfig.map((item, index) => (
