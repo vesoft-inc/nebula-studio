bcaa50b13ab86df42e0e7e539e4f9409d3e42fa4
Nut He <18328704+hetao92@users.noreply.github.com>
2022-02-17 12:00:38 +0800

    fix: fix env check & encode bug (#109)

--- scripts/rpm/preinst.sh
+++ scripts/rpm/preinst.sh
@@ -3,7 +3,3 @@ if ! type "lsof" > /dev/null; then
     echo -e "\e[31mInstall failed for the reason: the command lsof required\e[0m"
     exit 1
 fi
-if ! type "node" > /dev/null; then
-    echo -e "\e[31mInstall failed for the reason: the nodejs(>v10.16.0) required\e[0m"
-    exit 1
-fi
