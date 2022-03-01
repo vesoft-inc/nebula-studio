2658b4ffbbdf01ac536d112a17f3744e9f712ecd
Nut He <18328704+hetao92@users.noreply.github.com>
2022-03-01 18:44:47 +0800

    feat: init console page

--- app/stores/console.ts
+++ app/stores/console.ts
@@ -1,14 +1,36 @@
 import { makeAutoObservable, observable } from 'mobx';
 import service from '@app/config/service';
 
+// split from semicolon out of quotation marks
+const SEMICOLON_REG = /((?:[^;'"]*(?:"(?:\\.|[^"])*"|'(?:\\.|[^'])*')[^;'"]*)+)|;/;
+const splitQuery = (query: string) => {
+  const queryList = query.split(SEMICOLON_REG).filter(Boolean);
+  const paramList: string[] = [];
+  const gqlList: string[] = [];
+  queryList.forEach(query => {
+    const _query = query.trim();
+    if (_query.startsWith(':')) {
+      paramList.push(_query);
+    } else {
+      gqlList.push(_query);
+    }
+  });
+  return {
+    paramList,
+    gqlList,
+  };
+};
+
 export class ConsoleStore {
   runGQLLoading = false;
   currentGQL = 'SHOW SPACES;';
-  result = {};
-
+  results = [] as any;
+  paramsMap = null  as any
   constructor() {
     makeAutoObservable(this, {
-      result: observable.ref,
+      results: observable.ref,
+      paramsMap: observable,
+      currentGQL: observable,
     });
   }
 
@@ -16,11 +38,15 @@ export class ConsoleStore {
     Object.keys(param).forEach(key => (this[key] = param[key]));
   };
 
-  asyncRunGQL = async(gql: string) => {
+  runGQL = async(gql: string) => {
     this.update({ runGQLLoading: true });
     try {
-      const result = await service.execNGQL(
-        { gql },
+      const { gqlList, paramList } = splitQuery(gql);
+      const _results = await service.batchExecNGQL(
+        {
+          gqls: gqlList.filter(item => item !== ''),
+          paramList,
+        },
         {
           trackEventConfig: {
             category: 'console',
@@ -28,11 +54,33 @@ export class ConsoleStore {
           },
         },
       );
-      this.update({ result, currentGQL: gql });
+      const updateQuerys = paramList.filter(item => {
+        const reg = /^\s*:params/gim;
+        return !reg.test(item);
+      });
+      if (updateQuerys.length > 0) {
+        await this.getParams();
+      }
+      this.update({
+        results: [..._results.data, ...this.results],
+        currentGQL: gql,
+      });
     } finally {
       window.setTimeout(() => this.update({ runGQLLoading: false }), 300);
     }
   };
+
+  getParams = async () => {
+    const results = (await service.execNGQL(
+      {
+        gql: '',
+        paramList: [':params'],
+      },
+    )) as any;
+    this.update({
+      paramsMap: results.data?.localParams || {},
+    });
+  }
 }
 
 const consoleStore = new ConsoleStore();
--- server/pkg/webserver/controller/gateway.go
+++ server/pkg/webserver/controller/gateway.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/vesoft-inc/nebula-http-gateway/ccore/nebula/gateway/dao"
 	"github.com/vesoft-inc/nebula-http-gateway/ccore/nebula/types"
@@ -18,7 +19,16 @@ type execNGQLParams struct {
 	Gql       string              `json:"gql"`
 	ParamList types.ParameterList `json:"paramList"`
 }
+type batchExecNGQLParams struct {
+	Gqls      []string            `json:"gqls"`
+	ParamList types.ParameterList `json:"paramList"`
+}
 
+type gqlData struct {
+	gql     string
+	message string
+	data    map[string]interface{}
+}
 type connectDBParams struct {
 	Address  string `json:"address"`
 	Port     int    `json:"port"`
@@ -38,6 +48,54 @@ type importDataParams struct {
 type handleImportActionParams struct {
 	TaskId     string `json:"taskId"`
 	TaskAction string `json:"taskAction"`
+}
+
+func BatchExecNGQL(ctx iris.Context) base.Result {
+	params := new(batchExecNGQLParams)
+	err := ctx.ReadJSON(params)
+	if err != nil {
+		zap.L().Warn("execNGQLParams get fail", zap.Error(err))
+		return base.Response{
+			Code:    base.Error,
+			Message: err.Error(),
+		}
+	}
+	nsid := ctx.GetCookie("nsid")
+	data := make([]map[string]interface{}, 0)
+
+	for i := 0; i < len(params.Gqls); i++ {
+		gql := params.Gqls[i]
+		execute, _, err := dao.Execute(nsid, gql, make([]string, 0))
+		gqlRes := make(map[string]interface{})
+		gqlRes["gql"] = gql
+		if err != nil {
+			gqlRes["message"] = err.Error()
+			gqlRes["code"] = base.Error
+		} else {
+			gqlRes["code"] = base.Success
+		}
+		gqlRes["data"] = execute
+		data = append(data, gqlRes)
+	}
+
+	if len(params.ParamList) > 0 {
+		execute, _, err := dao.Execute(nsid, "", params.ParamList)
+		gqlRes := make(map[string]interface{})
+		gqlRes["gql"] = strings.Join(params.ParamList, "; ")
+		if err != nil {
+			gqlRes["message"] = err.Error()
+			gqlRes["code"] = base.Error
+		} else {
+			gqlRes["code"] = base.Success
+		}
+		gqlRes["data"] = execute
+		data = append(data, gqlRes)
+	}
+
+	return base.Response{
+		Code: base.Success,
+		Data: data,
+	}
 }
 
 func ExecNGQL(ctx iris.Context) base.Result {
