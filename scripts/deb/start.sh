c52a4970532a48e47fef4f307d2f59ddca52dfda
Nut He <18328704+hetao92@users.noreply.github.com>
2022-03-09 10:32:29 +0800

    feat: support open in explorer (#141)
    mod: code review

--- scripts/deb/start.sh
+++ scripts/deb/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p /usr/lib/systemd/system
 cd /usr/local/nebula-graph-studio/
 cp ./lib/nebula-graph-studio.service /usr/lib/systemd/system/
 sudo systemctl daemon-reload && sudo systemctl enable nebula-graph-studio.service && sudo systemctl start nebula-graph-studio.service
