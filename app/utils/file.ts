24adc6597afec5fd51b2b9380babdca0b8e186c1
Xiaolong Lu <bruce.lu@vesoft.com>
2022-06-30 16:36:10 +0800

    export error to local file (#245)
    * - feat: export error to local file
    - fix: remove useless nsid
    - feat: token name config
    
    * update: getFileSize

--- app/utils/file.ts
+++ app/utils/file.ts
@@ -2,20 +2,19 @@ export function readFileContent(file) {
   const reader = new FileReader();
   return new Promise((resolve, reject) => {
     reader.onload = (event: any) => resolve(event.target.result);
-    reader.onerror = error => reject(error);
+    reader.onerror = (error) => reject(error);
     reader.readAsText(file);
   });
 }
 
 export function getFileSize(size: number) {
-  const num = 1024;
-  if (size < num) {
-    return `${size} B`;
-  } else if (size < Math.pow(num, 2)) {
-    return `${(size / num).toFixed(2)} KB`;
-  } else if (size < Math.pow(num, 3)) {
-    return `${(size / Math.pow(num, 2)).toFixed(2)} MB`;
-  } else {
-    return `${(size / Math.pow(num, 3)).toFixed(2)} GB`;
+  const units = ['B', 'KB', 'MB', 'GB', 'TB'] as const;
+  const gap = 1 << 10;
+
+  for (let i = 0, byte = gap; i < units.length; i++, byte *= gap) {
+    if (size < byte || i === units.length - 1) {
+      const unitSize = ((size * gap) / byte).toFixed(2);
+      return `${unitSize} ${units[i]}` as `${number} ${typeof units[number]}`;
+    }
   }
 }
