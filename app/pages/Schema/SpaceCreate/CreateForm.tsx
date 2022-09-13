1225e3b478cd2b5333659d4c92f32a2097ff3e63
Nut He <18328704+hetao92@users.noreply.github.com>
2022-09-13 18:22:09 +0800

    mod: update space create form

---
+++ app/pages/Schema/SpaceCreate/CreateForm.tsx
@@ -0,0 +1,128 @@
+import { Col, Form, Input, Row, Select, FormProps } from 'antd';
+import React, { useMemo } from 'react';
+import { useStore } from '@app/stores';
+import { nameRulesFn, numberRulesFn, replicaRulesFn } from '@app/config/rules';
+import intl from 'react-intl-universal';
+import styles from './index.module.less';
+const Option = Select.Option;
+
+
+const defaultFormItemLayout = {
+  labelCol: {
+    span: 10,
+  },
+  wrapperCol: {
+    span: 11,
+  },
+};
+interface IProps extends FormProps {
+  className?: string;
+  formItemLayout?: typeof defaultFormItemLayout;
+  activeMachineNum: number;
+  colSpan: 'full' | 'half';
+}
+
+const CreateForm = (props: IProps) => {
+  const { onFieldsChange, onFinish, className, formItemLayout, form, colSpan } = props;
+  const { schema } = useStore();
+  const { activeMachineNum } = schema;
+  const _colSpan = useMemo(() => colSpan === 'full' ? 24 : 12, [colSpan])
+  const vidColSpans = useMemo(() => {
+    if(colSpan === 'full') {
+      return [14, 9];
+    } else {
+      return [formItemLayout.wrapperCol.span || 11, 11];
+    }
+  }, [colSpan])
+  return (
+    <Form 
+      className={className || styles.spaceForm} 
+      form={form} 
+      layout="vertical" 
+      onFieldsChange={onFieldsChange} 
+      onFinish={onFinish} 
+      {...defaultFormItemLayout} {...formItemLayout}>
+      <Row>
+        <Col span={_colSpan}>
+          <Form.Item label={intl.get('common.name')} name="name" rules={nameRulesFn()}>
+            <Input placeholder={intl.get('schema.spaceNameEnter')} />
+          </Form.Item>
+        </Col>
+        <Col span={_colSpan} className={styles.vidItem}>
+          <Form.Item noStyle shouldUpdate={true}>
+          {({ getFieldValue }) => {
+            const vidType = getFieldValue('vidType');
+            return <>
+              <Form.Item
+                label="Vid Type"
+                name="vidType"
+                rules={[{ required: true, message: intl.get('formRules.vidTypeRequired') }]}
+                wrapperCol={vidType === 'FIXED_STRING' && { span: vidColSpans[0]}}
+              >
+                <Select placeholder={intl.get('schema.selectVidTypeTip')}>
+                  <Option value="FIXED_STRING">FIXED_STRING</Option>
+                  <Option value="INT64">INT64</Option>
+                </Select>
+              </Form.Item>
+              {vidType === 'FIXED_STRING' 
+                ? <Col span={vidColSpans[1]} offset={vidColSpans[0] + 1} className={styles.stringLength}>
+                    <Form.Item label={intl.get('schema.length')} name="stringLength" rules={[
+                      {
+                        required: true,
+                        message: intl.get('formRules.fixStringLengthRequired'),
+                      },
+                      ...numberRulesFn(),
+                    ]}>
+                      <Input />
+                    </Form.Item>
+                  </Col>
+                : null}
+            </>
+          }}
+          </Form.Item>
+        
+        </Col>
+      </Row>
+      <Row>
+        <Col span={_colSpan}>
+          <Form.Item label={<span>
+            {intl.get('common.comment')}:
+            <span className={styles.optionalItem}>({intl.get('common.optional')})</span>
+          </span>} name="comment">
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={_colSpan}>
+          <Form.Item
+            colon={false}
+            label={<span>
+              Partition_num:
+              <span className={styles.optionalItem}>({intl.get('common.optional')})</span>
+            </span>}
+            name="partitionNum"
+            rules={numberRulesFn()}
+          >
+            <Input placeholder="100" />
+          </Form.Item>
+        </Col>
+        <Col span={_colSpan}>
+          <Form.Item
+            colon={false}
+            label={<span>
+              Replica_factor:
+              <span className={styles.optionalItem}>({intl.get('common.optional')})</span>
+            </span>}
+            name="replicaFactor"
+            rules={replicaRulesFn(activeMachineNum)}
+          >
+            <Input placeholder="1" />
+          </Form.Item>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default CreateForm;
--- app/pages/Schema/SpaceCreate/index.tsx
+++ app/pages/Schema/SpaceCreate/index.tsx
@@ -1,19 +1,17 @@
-import { Button, Col, Form, Input, Row, Select, message } from 'antd';
+import { Button, Form, message } from 'antd';
 import React, { useEffect, useMemo, useState } from 'react';
 import Breadcrumb from '@app/components/Breadcrumb';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '@app/stores';
 import { trackPageView } from '@app/utils/stat';
-import { nameRulesFn, numberRulesFn, replicaRulesFn } from '@app/config/rules';
 import { getSpaceCreateGQL } from '@app/utils/gql';
 import GQLCodeMirror from '@app/components/GQLCodeMirror';
 import intl from 'react-intl-universal';
 import cls from 'classnames';
 import { useHistory } from 'react-router-dom';
-import FormItem from 'antd/lib/form/FormItem';
 import Cookie from 'js-cookie';
 import styles from './index.module.less';
-const Option = Select.Option;
+import CreateForm from './CreateForm';
 
 function getVidType(type: string, length?: string) {
   let result;
@@ -99,79 +97,14 @@ const SpaceCreate = () => {
     <div className={styles.spaceCreate}>
       <Breadcrumb routes={routes} />
       <div className={cls(styles.configContainer, 'studioCenterLayout')}>
-        <Form className={styles.spaceForm} form={form} layout="vertical" onFieldsChange={updateGql} {...formItemLayout}>
-          <Row>
-            <Col span={12}>
-              <Form.Item label={intl.get('common.name')} name="name" rules={nameRulesFn()}>
-                <Input placeholder={intl.get('schema.spaceNameEnter')} />
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                label="Vid Type"
-                name="vidType"
-                rules={[{ required: true }]}
-              >
-                <Select placeholder={intl.get('schema.selectVidTypeTip')}>
-                  <Option value="FIXED_STRING">FIXED_STRING</Option>
-                  <Option value="INT64">INT64</Option>
-                </Select>
-              </Form.Item>
-              <FormItem noStyle dependencies={['vidType']}>
-                {({ getFieldValue }) => {
-                  const vidType = getFieldValue('vidType');
-                  return vidType === 'FIXED_STRING' ? <Col offset={11} className={styles.stringLength}>
-                    <Form.Item label={intl.get('schema.length')} name="stringLength" rules={[
-                      {
-                        required: true,
-                        message: 'fix string length limit is required',
-                      },
-                      ...numberRulesFn(),
-                    ]}>
-                      <Input />
-                    </Form.Item>
-                  </Col>
-                    : null;
-                }}
-              </FormItem>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={12}>
-              <Form.Item label={intl.get('common.comment')} name="comment">
-                <Input />
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={12}>
-              <Form.Item
-                colon={false}
-                label={<span>
-                  Partition_num:
-                  <span className={styles.optionalItem}>({intl.get('common.optional')})</span>
-                </span>}
-                name="partitionNum"
-                rules={numberRulesFn()}
-              >
-                <Input placeholder="100" />
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                colon={false}
-                label={<span>
-                  Replica_factor:
-                  <span className={styles.optionalItem}>({intl.get('common.optional')})</span>
-                </span>}
-                name="replicaFactor"
-                rules={replicaRulesFn(activeMachineNum)}
-              >
-                <Input placeholder="1" />
-              </Form.Item>
-            </Col>
-          </Row>
-        </Form>
+        <CreateForm 
+          form={form}
+          colSpan='half'
+          onFieldsChange={updateGql} 
+          className={styles.spaceForm} 
+          formItemLayout={formItemLayout} 
+          activeMachineNum={activeMachineNum}
+        />
         <GQLCodeMirror currentGQL={gql} />
       </div>
       <div className="studioFormFooter">
