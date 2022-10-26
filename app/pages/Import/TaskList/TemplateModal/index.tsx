6ba8a812cbce4ca768d2fd3a7ab78aa505ff0052
Nut He <18328704+hetao92@users.noreply.github.com>
2022-10-26 18:00:34 +0800

    mod: code review

--- app/pages/Import/TaskList/TemplateModal/index.tsx
+++ app/pages/Import/TaskList/TemplateModal/index.tsx
@@ -38,7 +38,7 @@ const TemplateModal = (props: IProps) => {
       const parseContent = yaml.load(content);
       if(typeof parseContent === 'object') {
         const connection = parseContent.clientSettings?.connection || {};
-        if(connection.address !== host.replace(/^https?:\/\//, '')) {
+        if(connection.address !== host) {
           throw new Error(intl.get('import.templateMatchError', { type: 'address' }));
         }
         if(connection.user !== username) {
