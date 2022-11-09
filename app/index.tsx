56af2f3342cef4bd1ba3e940d7b0966400df4e27
Nut He <18328704+hetao92@users.noreply.github.com>
2022-11-09 13:54:26 +0800

    mod: add track (#347)

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { hot } from 'react-hot-loader/root';
 import { Spin, message } from 'antd';
-import React, { Suspense, lazy, useState } from 'react';
+import React, { Suspense, lazy, useState, useEffect } from 'react';
 import ReactDom from 'react-dom';
 import { Route, BrowserRouter as Router, Switch, useHistory } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
@@ -8,8 +8,10 @@ import dayjs from 'dayjs';
 import intl from 'react-intl-universal';
 import duration from 'dayjs/plugin/duration';
 import Cookie from 'js-cookie';
+import { trackPageView } from '@app/utils/stat';
 import { INTL_LOCALES } from '@app/config/constants';
 import { LanguageContext } from '@app/context';
+import { useStore } from '@app/stores';
 import AuthorizedRoute from './AuthorizedRoute';
 import rootStore, { StoreProvider } from './stores';
 const Login = lazy(() => import('@app/pages/Login'));
@@ -29,6 +31,7 @@ intl.init({
 
 
 const PageRoot = observer(() => {
+  const { global: { version } } = useStore();
   const [currentLocale, setCurrentLocale] = useState(
     defaultLanguage || 'EN-US',
   );
@@ -42,6 +45,10 @@ const PageRoot = observer(() => {
         locales: INTL_LOCALES,
       });
   };
+
+  useEffect(() => {
+    trackPageView(`Studio/v${version}`);
+  }, []);
 
   return (
     <StoreProvider value={rootStore}>
