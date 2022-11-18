1c8f30af45ba31406e7fa664344f840ae354e03a
Nut He <18328704+hetao92@users.noreply.github.com>
2022-11-18 11:05:08 +0800

    mod: code review

--- app/components/ErrorBoundary/index.tsx
+++ app/components/ErrorBoundary/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import errorImg from '@app/static/images/errorBoundary.png';
-import { I18nContext } from '@vesoft-inc/i18n';
+import { I18nContext, useI18n } from '@vesoft-inc/i18n';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Button, message } from 'antd';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
@@ -10,6 +10,33 @@ interface IProps extends RouteComponentProps {}
 interface IState {
   errInfo: string;
 }
+
+function ErrorFallback(props: { errInfo: string }) {
+  const { errInfo } = props;
+  const { intl } = useI18n();
+  const handleCopy = useCallback(() => {
+    message.success(intl.get('common.copySuccess'));
+  }, []);
+  return <div className={styles.errPage}>
+    <img src={errorImg} className={styles.errImg} />
+    <p className={styles.errText}>{intl.get('warning.crashPage')}</p>
+    <p className={styles.errTip}>{intl.get('warning.crashPageTip')}</p>
+    <div className={styles.btns}>
+      <Button type="primary" onClick={() => window.location.reload()}>{intl.get('warning.refreshPage')}</Button>
+      <Button onClick={() => window.open(intl.get('link.forumHref'), '_blank')}>{intl.get('warning.contactStaff')}</Button>
+    </div>
+    <div className={styles.errContainer}>
+      <div className={styles.header}>
+        <span>{intl.get('warning.errorMessage')}</span>
+        <CopyToClipboard key={1} text={errInfo} onCopy={handleCopy}>
+          <Button>{intl.get('common.copy')}</Button>
+        </CopyToClipboard>
+      </div>
+      <p className={styles.errMsg}>{errInfo.toString()}</p>
+    </div>
+  </div>;
+}
+
 class ErrorBoundary extends React.PureComponent<IProps, IState> {
   constructor(props) {
     super(props);
@@ -31,33 +58,10 @@ class ErrorBoundary extends React.PureComponent<IProps, IState> {
     }
   }
 
-  handleCopy = () => {
-    const { intl } = this.context;
-    message.success(intl.get('common.copySuccess'));
-  };
-
   render() {
     const { errInfo } = this.state;
-    const { intl } = this.context;
     if(!errInfo) return this.props.children;
-    return <div className={styles.errPage}>
-      <img src={errorImg} className={styles.errImg} />
-      <p className={styles.errText}>{intl.get('warning.crashPage')}</p>
-      <p className={styles.errTip}>{intl.get('warning.crashPageTip')}</p>
-      <div className={styles.btns}>
-        <Button type="primary" onClick={() => window.location.reload()}>{intl.get('warning.refreshPage')}</Button>
-        <Button onClick={() => window.open(intl.get('link.forumHref'), '_blank')}>{intl.get('warning.contactStaff')}</Button>
-      </div>
-      <div className={styles.errContainer}>
-        <div className={styles.header}>
-          <span>{intl.get('warning.errorMessage')}</span>
-          <CopyToClipboard key={1} text={errInfo} onCopy={this.handleCopy}>
-            <Button>{intl.get('common.copy')}</Button>
-          </CopyToClipboard>
-        </div>
-        <p className={styles.errMsg}>{errInfo.toString()}</p>
-      </div>
-    </div>;
+    return <ErrorFallback errInfo={errInfo} />;
   }
 }
 
