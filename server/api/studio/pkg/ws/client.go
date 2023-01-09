b5cc90181b19639fe24933adca0440af7ae34773
Bruce <bruce.lu@vesoft.com>
2023-01-09 13:56:41 +0800

    fix: websocket message send

--- server/api/studio/pkg/ws/client.go
+++ server/api/studio/pkg/ws/client.go
@@ -251,13 +251,6 @@ func (c *Client) writePump() {
 			}
 			w.Write(message)
 
-			// Add queued chat messages to the current websocket message.
-			n := len(c.send)
-			for i := 0; i < n; i++ {
-				w.Write(newline)
-				w.Write(<-c.send)
-			}
-
 			if err := w.Close(); err != nil {
 				return
 			}
