6d10007d59ccb0e4873787f3e365857b0f6acc7a
hetao92 <18328704+hetao92@users.noreply.github.com>
2023-03-30 03:45:03 +0000

    feat: add template validate tips

--- app/pages/Import/TaskList/TemplateModal/index.tsx
+++ app/pages/Import/TaskList/TemplateModal/index.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-template-curly-in-string */
 import { Button, Form, Input, Modal, Spin, Upload, message } from 'antd';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import Icon from '@app/components/Icon';
 import { Link } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
@@ -26,34 +27,122 @@ const TemplateModal = (props: IProps) => {
   const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
   const [config, setConfig] = useState('');
-  const { intl } = useI18n();
+  const { intl, currentLocale } = useI18n();
   const { dataImport: { importTask }, files: { getFiles, fileList } } = useStore();
+  const files = useMemo(() => fileList.map(item => item.name), [fileList]);
   useEffect(() => {
     getFiles();
   }, []);
+  const emptyTextMap = useMemo(() => ({
+    address: {
+      text: '${YOUR_NEBULA_ADDRESS}',
+      message: intl.get('import.addressRequired'),
+    },
+    user: {
+      text: '${YOUR_NEBULA_NAME}',
+      message: intl.get('import.usernameRequired'),
+    },
+    password: {
+      text: '${YOUR_NEBULA_PASSWORD}',
+      message: intl.get('import.passwordRequired'),
+    },
+    s3AccessKey: {
+      text: '${YOUR_S3_ACCESS_KEY}',
+      message: intl.get('import.s3AccessKeyRequired'),
+    },
+    s3SecretKey: {
+      text: '${YOUR_S3_SECRET_KEY}',
+      message: intl.get('import.s3SecretKeyRequired'),
+    },
+    sftpUsername: {
+      text: '${YOUR_SFTP_USER}',
+      message: intl.get('import.sftpUsernameRequired'),
+    },
+    sftpPassword: {
+      text: '${YOUR_SFTP_PASSWORD}',
+      message: intl.get('import.sftpPasswordRequired'),
+    },
+    ossAccessKey: {
+      text: '${YOUR_OSS_ACCESS_KEY}',
+      message: intl.get('import.ossAccessKeyRequired'),
+    },
+    ossSecretKey: {
+      text: '${YOUR_OSS_SECRET_KEY}',
+      message: intl.get('import.ossSecretKeyRequired'),
+    },
+  }), [currentLocale]);
+  const validateEmpty = useCallback((key, value) => {
+    if(!value || value === emptyTextMap[key].text) {
+      return emptyTextMap[key].message;
+    }
+  }, []);
+
+  const validateAddress = useCallback((client) => {
+    const msg = validateEmpty('address', client.address);
+    console.log('msg', msg);
+    if(msg) return msg;
+    const address = client.address.split(',');
+    if(address.some(i => i.startsWith('http'))) {
+      return intl.get('import.noHttp');
+    }
+    if (!address.includes(host)) {
+      return intl.get('import.addressMatch');
+    }
+  }, [currentLocale]);
+
+  const validateUser = useCallback((client) => {
+    const msg = validateEmpty('user', client.user);
+    if(msg) return msg;
+    if (client.user !== username) {
+      return intl.get('import.templateMatchError');
+    }
+  }, [currentLocale, username]);
+  const validatePassword = useCallback((client) => validateEmpty('password', client.password), []);
+  const validateS3 = useCallback((s3) => validateEmpty('s3AccessKey', s3.accessKey) || validateEmpty('s3SecretKey', s3.secretKey), []);
+  const validateSftp = useCallback((sftp) => validateEmpty('sftpUsername', sftp.username) || validateEmpty('sftpPassword', sftp.password), []);
+  const validateOSS = useCallback((oss) => validateEmpty('ossAccessKey', oss.accessKey) || validateEmpty('ossSecretKey', oss.secretKey), []);
+  const validateClient = useCallback((content) => {
+    const client = content.client || {};
+    return validateAddress(client) || validateUser(client) || validatePassword(client);
+  }, [validateAddress, validateUser, validatePassword]);
+  const validateSources = useCallback((content) => {
+    const sources = content.sources || [];
+    let err;
+    sources.some((file) => {
+      if(file.path && !files.includes(file.path)) {
+        err = intl.get('import.fileNotExist', { name: file.path });
+        return !!err;
+      }
+      if(file.s3) {
+        err = validateS3(file.s3);
+        return !!err;
+      }
+      if(file.sftp) {
+        err = validateSftp(file.sftp);
+        return !!err;
+      }
+      if(file.oss) {
+        err = validateOSS(file.oss);
+        return !!err;
+      }
+    });
+    return err;
+  }, [validateS3, validateSftp, validateOSS, files]);
+
   const handleFileImport = async ({ file }) => {
     await setLoading(true);
-    const files = fileList.map(item => item.name);
     const content = await readFileContent(file);
     try {
       const parseContent = yaml.load(content);
       if(typeof parseContent === 'object') {
-        const client = parseContent.client || {};
-        const address = client.address.split(',');
-        if(client.address.startsWith('http')) {
-          throw new Error(intl.get('import.noHttp'));
-        }
-        if(!address.includes(host)) {
-          throw new Error(intl.get('import.addressMatch'));
-        }
-        if(client.user !== username) {
-          throw new Error(intl.get('import.templateMatchError', { type: 'username' }));
-        }
-        parseContent.sources?.forEach(file => {
-          if(file.path && !files.includes(file.path)) {
-            throw new Error(intl.get('import.fileNotExist', { name: file.path }));
-          }
+        let err;
+        [validateClient, validateSources].some((strategy) => {
+          err = strategy(parseContent);
+          return !!err;
         });
+        if(err) {
+          throw new Error(err);
+        }
         // empty props in yaml will converted to null, but its required in nebula-importer
         parseContent.sources.forEach(file => {
           if(file.edges) {
