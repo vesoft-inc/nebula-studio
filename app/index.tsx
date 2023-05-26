75237140a92a8ff1d03a07cfab3202c63f675f84
Bruce <bruce.lu@vesoft.com>
2023-05-26 19:33:16 +0800

    feat: antd@5

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import { hot } from 'react-hot-loader/root';
-import { Spin, message } from 'antd';
+import { Spin, message, ConfigProvider } from 'antd';
+import enUS from 'antd/locale/en_US';
+import zhCN from 'antd/locale/zh_CN';
 import React, { Suspense, lazy, useEffect } from 'react';
 import ReactDom from 'react-dom';
 import { Route, BrowserRouter as Router, Switch, useHistory } from 'react-router-dom';
@@ -8,25 +10,32 @@ import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import Cookie from 'js-cookie';
 import { trackPageView } from '@app/utils/stat';
-import { I18nProvider, getI18n } from '@vesoft-inc/i18n';
+import { I18nProvider, getI18n, useI18n } from '@vesoft-inc/i18n';
 import { INTL_LOCALES } from '@app/config/constants';
 import { useStore } from '@app/stores';
 import AuthorizedRoute from './AuthorizedRoute';
 import rootStore, { StoreProvider } from './stores';
+
+import 'dayjs/locale/zh-cn';
+import './utils/reset.css';
+import './common.less';
+import './app.less';
+
+dayjs.extend(duration);
+message.config({ maxCount: 1 });
+
 const Login = lazy(() => import('@app/pages/Login'));
 const MainPage = lazy(() => import('@app/pages/MainPage'));
 
-import './common.less';
-import './app.less';
-dayjs.extend(duration);
-message.config({
-  maxCount: 1,
-});
 const defaultLanguage = Cookie.get('lang') || document.documentElement.getAttribute('lang');
 const { initI18n } = getI18n();
 initI18n(defaultLanguage, INTL_LOCALES);
+dayjs.locale(defaultLanguage === 'EN_US' ? 'en' : 'zh-cn');
+
 const PageRoot = observer(() => {
-  const { global: { version } } = useStore();
+  const {
+    global: { version },
+  } = useStore();
   useEffect(() => {
     trackPageView(`Studio/v${version}`);
   }, []);
@@ -44,15 +53,36 @@ const PageRoot = observer(() => {
 
 const App = () => {
   const history = useHistory();
+  const { currentLocale } = useI18n();
   rootStore.global.history = history;
 
+  useEffect(() => {
+    dayjs.locale(currentLocale === 'EN_US' ? 'en' : 'zh-cn');
+  }, [currentLocale]);
+
   return (
-    <Switch>
-      <Suspense fallback={<Spin />}>
-        <Route path="/login" exact={true} component={Login} />
-        <AuthorizedRoute component={MainPage} />
-      </Suspense>
-    </Switch>
+    <ConfigProvider
+      locale={currentLocale === 'EN_US' ? enUS : zhCN}
+      theme={{
+        token: {
+          controlHeight: 38,
+          colorPrimary: '#0091ff',
+          fontFamily: 'Robot-Regular, sans-serif',
+        },
+        components: {
+          Menu: {
+            colorItemBg: '#2f3a4a',
+          },
+        },
+      }}
+    >
+      <Switch>
+        <Suspense fallback={<Spin />}>
+          <Route path="/login" exact={true} component={Login} />
+          <AuthorizedRoute component={MainPage} />
+        </Suspense>
+      </Switch>
+    </ConfigProvider>
   );
 };
 
