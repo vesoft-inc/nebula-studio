dc19ec7ac04f25ca9d393c10245d2e76625adb00
Bruce <bruce.lu@vesoft.com>
2023-07-04 18:28:33 +0800

    mod: remove bank line

--- server/api/studio/pkg/filestore/s3store_test.go
+++ server/api/studio/pkg/filestore/s3store_test.go
@@ -7,7 +7,6 @@ import (
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
-
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"github.com/stretchr/testify/assert"
