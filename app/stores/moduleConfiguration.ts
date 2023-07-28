0b64b3bd6ab6fe856585bbca0ef9a0c39793be09
Nut He <18328704+hetao92@users.noreply.github.com>
2023-07-28 16:18:36 +0800

    fix: fix configuration interface

--- app/stores/moduleConfiguration.ts
+++ app/stores/moduleConfiguration.ts
@@ -1,7 +1,21 @@
-import { makeAutoObservable } from 'mobx';
 import { ES3Platform, IDatasourceType } from '@app/interfaces/datasource';
 
-export class ModuleConfigurationStore {
+export interface IModuleConfigurationStore {
+  readonly schema?: {
+    supportCreateSpace?: boolean;
+  };
+  readonly dataImport?: {
+    supportTemplate?: boolean;
+    supportConfigDownload?: boolean;
+    supportLogDownload?: boolean;
+    needPwdConfirm?: boolean;
+    supportDatasourceType?: IDatasourceType[];
+    supportS3Platform?: ES3Platform[];
+    supportMoreConfig?: boolean;
+  };
+}
+
+export class ModuleConfigurationStore implements IModuleConfigurationStore {
   schema = {
     supportCreateSpace: true,
   };
@@ -14,9 +28,6 @@ export class ModuleConfigurationStore {
     supportS3Platform: [ES3Platform.AWS, ES3Platform.OSS, ES3Platform.Tecent, ES3Platform.Customize],
     supportMoreConfig: true,
   };
-  constructor() {
-    makeAutoObservable(this);
-  }
 }
 
 const moduleConfigurationStore = new ModuleConfigurationStore();
