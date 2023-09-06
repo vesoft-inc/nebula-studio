d55a6853980c28ece2c8d8f65b064643a16daa1b
Bruce <bruce.lu@vesoft.com>
2023-09-06 20:42:20 +0800

    fix: empty space result in Console

--- app/pages/Console/OutputBox/index.tsx
+++ app/pages/Console/OutputBox/index.tsx
@@ -20,16 +20,8 @@ interface IProps {
   index: number;
   result: HistoryResult;
   onHistoryItem: (gql: string, space?: string) => void;
-  onExplorer?: (params: {
-    space: string;
-    vertexes: any[], 
-    edges: any[]
-  }) => void;
-  onResultConfig?: (data: {
-    space: string;
-    spaceVidType: string;
-    [key: string]: any;
-  }) => void;
+  onExplorer?: (params: { space: string; vertexes: any[]; edges: any[] }) => void;
+  onResultConfig?: (data: { space: string; spaceVidType: string; [key: string]: any }) => void;
   templateRender?: (data) => JSX.Element;
 }
 
@@ -39,7 +31,7 @@ const OutputBox = (props: IProps) => {
   const { intl } = useI18n();
   const [visible, setVisible] = useState(true);
   const { results, update, favorites, saveFavorite, deleteFavorite, getFavoriteList } = console;
-  const { code, data, message, gql, space, spaceVidType } = result; 
+  const { code, data, message, gql, space, spaceVidType } = result;
   const [columns, setColumns] = useState<any>([]);
   const [dataSource, setDataSource] = useState<any>([]);
   const [isFavorited, setIsFavorited] = useState(false);
@@ -49,19 +41,19 @@ const OutputBox = (props: IProps) => {
   const initData = useCallback(() => {
     let _columns = [] as any;
     let _dataSource = [] as any;
-    if(!data) {
+    if (!data) {
       return;
     }
     const { headers, tables, localParams } = data;
     if (tables.length > 0) {
       _dataSource = data.tables;
-      setShowGraph(_dataSource.filter(item => 
-        item._verticesParsedList ||
-        item._edgesParsedList ||
-        item._pathsParsedList).length > 0);
-    } 
-    if(headers.length > 0) {
-      _columns = data.headers.map(column => {
+      setShowGraph(
+        _dataSource.filter((item) => item._verticesParsedList || item._edgesParsedList || item._pathsParsedList)
+          .length > 0,
+      );
+    }
+    if (headers.length > 0) {
+      _columns = data.headers.map((column) => {
         return {
           title: column,
           dataIndex: column,
@@ -71,13 +63,10 @@ const OutputBox = (props: IProps) => {
             return v1 === v2 ? 0 : v1 > v2 ? 1 : -1;
           },
           sortDirections: ['descend', 'ascend'],
-          render: value => {
+          render: (value) => {
             if (typeof value === 'boolean') {
               return value.toString();
-            } else if (
-              typeof value === 'number' ||
-              BigNumber.isBigNumber(value)
-            ) {
+            } else if (typeof value === 'number' || BigNumber.isBigNumber(value)) {
               return value.toString();
             }
             return value;
@@ -87,21 +76,16 @@ const OutputBox = (props: IProps) => {
     }
     if (localParams) {
       const params = {};
-      Object.entries(data?.localParams).forEach(
-        ([k, v]) => (params[k] = JSON.stringify(v)),
-      );
+      Object.entries(data?.localParams).forEach(([k, v]) => (params[k] = JSON.stringify(v)));
       _dataSource = [{ ...params }];
-      _columns = Object.keys(data?.localParams).map(column => {
+      _columns = Object.keys(data?.localParams).map((column) => {
         return {
           title: column,
           dataIndex: column,
-          render: value => {
+          render: (value) => {
             if (typeof value === 'boolean') {
               return value.toString();
-            } else if (
-              typeof value === 'number' ||
-              BigNumber.isBigNumber(value)
-            ) {
+            } else if (typeof value === 'number' || BigNumber.isBigNumber(value)) {
               return value.toString();
             }
             return value;
@@ -112,52 +96,49 @@ const OutputBox = (props: IProps) => {
     setDataSource(_dataSource);
     setColumns(_columns);
   }, []);
-  
+
   useEffect(() => {
     initData();
   }, []);
 
-  const handleTabChange = useCallback(key => {
+  const handleTabChange = useCallback((key) => {
     setTab(key);
     trackEvent('console', `change_tab_${key}`);
   }, []);
 
   const addFavorites = useCallback(async () => {
-    if(!gql) {
+    if (!gql) {
       return;
     }
     await saveFavorite(gql);
     getFavoriteList();
   }, [gql]);
   const removeFavorite = useCallback(async () => {
-    const id = console.favorites.find(item => item.content === gql)?.id;
+    const id = console.favorites.find((item) => item.content === gql)?.id;
     await deleteFavorite(id);
     getFavoriteList();
   }, []);
 
-  useEffect(() => { 
-    setIsFavorited(favorites.filter(item => item.content === gql).length > 0);
+  useEffect(() => {
+    setIsFavorited(favorites.filter((item) => item.content === gql).length > 0);
   }, [favorites, gql]);
 
   const handleItemRemove = useCallback(() => {
     update({
-      results: results.filter((_, i) => i !== index)
+      results: results.filter((_, i) => i !== index),
     });
   }, [results, index]);
 
   const downloadCsv = () => {
     if (!data) {
-      return ;
+      return;
     }
     let url = '#';
     const { headers = [], tables = [] } = data;
-    const csv = [
-      headers,
-      ...tables.map(values => headers.map(field => values[field])),
-    ]
-      .map(row =>
+    const csv = [headers, ...tables.map((values) => headers.map((field) => values[field]))]
+      .map((row) =>
         // HACK: waiting for use case if there need to check int or string
-        row.map(value => `"${value.toString().replace(/"/g, '""')}"`).join(','),
+        row.map((value) => `"${value.toString().replace(/"/g, '""')}"`).join(','),
       )
       .join('\n');
     if (!csv) {
@@ -179,7 +160,7 @@ const OutputBox = (props: IProps) => {
   };
 
   const downloadPng = () => {
-    if(graph) {
+    if (graph) {
       let canvas = graph.twoGraph.canvas;
       const shadowCanvas = document.createElement('canvas');
       shadowCanvas.width = canvas.width;
@@ -190,7 +171,7 @@ const OutputBox = (props: IProps) => {
       ctx.drawImage(canvas, 0, 0);
       canvas = shadowCanvas;
       setTimeout(() => {
-        canvas.toBlob(blob => {
+        canvas.toBlob((blob) => {
           const url = URL.createObjectURL(blob);
           const a = document.createElement('a');
           a.href = canvas.toDataURL('image/png');
@@ -205,12 +186,8 @@ const OutputBox = (props: IProps) => {
 
   const handleExplore = () => {
     if (
-      data.tables.filter(
-        item =>
-          item._verticesParsedList ||
-          item._edgesParsedList ||
-          item._pathsParsedList,
-      ).length > 0
+      data.tables.filter((item) => item._verticesParsedList || item._edgesParsedList || item._pathsParsedList).length >
+      0
     ) {
       parseToGraph();
     } else {
@@ -230,121 +207,139 @@ const OutputBox = (props: IProps) => {
     const { vertexes, edges } = parseSubGraph(data.tables, spaceVidType);
     onExplorer!({
       space,
-      vertexes, 
-      edges
+      vertexes,
+      edges,
     });
   };
   const resultSuccess = useMemo(() => code === 0, [code]);
   const items = [
     resultSuccess && {
       key: 'table',
-      label: <>
-        <Icon type="icon-studio-console-table" />
-        {intl.get('common.table')}
-      </>,
-      children: <Table
-        bordered={true}
-        columns={columns}
-        dataSource={dataSource}
-        pagination={{
-          showTotal: () =>
-            `${intl.get('common.total')} ${dataSource.length}`,
-        }}
-        rowKey={() => uuidv4()}
-      />
+      label: (
+        <>
+          <Icon type="icon-studio-console-table" />
+          {intl.get('common.table')}
+        </>
+      ),
+      children: (
+        <Table
+          bordered={true}
+          columns={columns}
+          dataSource={dataSource}
+          pagination={{
+            showTotal: () => `${intl.get('common.total')} ${dataSource.length}`,
+          }}
+          rowKey={() => uuidv4()}
+        />
+      ),
     },
-    resultSuccess && data.headers[0] === 'format' && {
-      key: 'graphViz',
-      label: <>
-        <Icon type="icon-studio-console-graphviz" />
-        {intl.get('console.graphviz')}
-      </>,
-      children: <Graphviz graph={dataSource[0]?.format} index={index} />
-    },
+    resultSuccess &&
+      data.headers[0] === 'format' && {
+        key: 'graphViz',
+        label: (
+          <>
+            <Icon type="icon-studio-console-graphviz" />
+            {intl.get('console.graphviz')}
+          </>
+        ),
+        children: <Graphviz graph={dataSource[0]?.format} index={index} />,
+      },
     showGraph && {
       key: 'graph',
-      label: <>
-        <Icon type="icon-studio-console-graph" />
-        {intl.get('common.graph')}
-      </>,
-      children: <ForceGraph space={space} data={dataSource} spaceVidType={spaceVidType} onGraphInit={setGraph} />
+      label: (
+        <>
+          <Icon type="icon-studio-console-graph" />
+          {intl.get('common.graph')}
+        </>
+      ),
+      children: <ForceGraph space={space} data={dataSource} spaceVidType={spaceVidType} onGraphInit={setGraph} />,
     },
     !resultSuccess && {
       key: 'log',
-      label: <>
-        <Icon type="icon-studio-console-logs" />
-        {intl.get('common.log')}
-      </>,
-      children: <div className={styles.errContainer}>{message}</div>
-    }
+      label: (
+        <>
+          <Icon type="icon-studio-console-logs" />
+          {intl.get('common.log')}
+        </>
+      ),
+      children: <div className={styles.errContainer}>{message}</div>,
+    },
   ].filter(Boolean);
-  return <div className={styles.outputBox}>
-    <div className={styles.outputHeader}>
-      <p className={cls(styles.gql, { [styles.errorInfo]: !resultSuccess })} onClick={() => onHistoryItem(gql, space)}>
-        <span className={styles.gqlValue}>{space ? `[${space}]>` : '$'} {gql}</span>
-      </p>
-      <div className={styles.outputOperations}>
-        {templateRender?.(gql)}
-        {!isFavorited ? <Tooltip title={intl.get('console.addToFavorites')} placement="top">
-          <Icon
-            type="icon-studio-btn-save"
-            onClick={addFavorites}
-          />
-        </Tooltip> : <Tooltip title={intl.get('console.unfavorite')} placement="top">
-          <Icon
-            className={styles.btnYellow}
-            type="icon-studio-btn-save-fill"
-            onClick={removeFavorite}
-          />
-        </Tooltip>}
-        <Popover
-          overlayClassName={styles.exportPopover}
-          placement="bottom"
-          content={<>
-            <Button type="link" className={styles.downloadItem} onClick={downloadCsv}>
-              {intl.get('schema.csvDownload')}
-            </Button>
-            <Button disabled={!graph || tab !== 'graph'} type="link" className={styles.downloadItem} onClick={downloadPng}>
-              {intl.get('schema.pngDownload')}
-            </Button>
-          </>}
+  return (
+    <div className={styles.outputBox}>
+      <div className={styles.outputHeader}>
+        <p
+          className={cls(styles.gql, { [styles.errorInfo]: !resultSuccess })}
+          onClick={() => onHistoryItem(gql, space)}
         >
-          <Icon className={styles.btnExport} type="icon-studio-btn-output" />
-        </Popover>
-        <Icon
-          type={visible ? 'icon-studio-btn-up' : 'icon-studio-btn-down'}
-          onClick={() => setVisible(!visible)}
-        />
-        <Icon
-          type="icon-studio-btn-close"
-          onClick={handleItemRemove}
-        />
-      </div>
-    </div>
-    {visible && <> 
-      <div className={styles.tabContainer}>
-        <Tabs
-          className={styles.outputTab}
-          defaultActiveKey={'log'}
-          size={'large'}
-          tabPosition={'left'}
-          onChange={handleTabChange}
-          items={items}
-        />
-      </div>
-      {resultSuccess && data.timeCost !== undefined && (
-        <div className={styles.outputFooter} onClick={handleRemoveMenu}>
-          <span>
-            {`${intl.get('console.execTime')} ${data.timeCost /
-              1000000} (s)`}
+          <span className={styles.gqlValue}>
+            {space ? `[${space}]>` : '$'} {gql}
           </span>
-          <div className={styles.btns}>
-            {onExplorer && <Button className="primaryBtn" type="text" onClick={handleExplore}>{intl.get('common.openInExplore')}</Button>}
-          </div>
+        </p>
+        <div className={styles.outputOperations}>
+          {templateRender?.(gql)}
+          {!isFavorited ? (
+            <Tooltip title={intl.get('console.addToFavorites')} placement="top">
+              <Icon type="icon-studio-btn-save" onClick={addFavorites} />
+            </Tooltip>
+          ) : (
+            <Tooltip title={intl.get('console.unfavorite')} placement="top">
+              <Icon className={styles.btnYellow} type="icon-studio-btn-save-fill" onClick={removeFavorite} />
+            </Tooltip>
+          )}
+          <Popover
+            overlayClassName={styles.exportPopover}
+            placement="bottom"
+            content={
+              <>
+                <Button type="link" className={styles.downloadItem} onClick={downloadCsv}>
+                  {intl.get('schema.csvDownload')}
+                </Button>
+                <Button
+                  disabled={!graph || tab !== 'graph'}
+                  type="link"
+                  className={styles.downloadItem}
+                  onClick={downloadPng}
+                >
+                  {intl.get('schema.pngDownload')}
+                </Button>
+              </>
+            }
+          >
+            <Icon className={styles.btnExport} type="icon-studio-btn-output" />
+          </Popover>
+          <Icon type={visible ? 'icon-studio-btn-up' : 'icon-studio-btn-down'} onClick={() => setVisible(!visible)} />
+          <Icon type="icon-studio-btn-close" onClick={handleItemRemove} />
         </div>
+      </div>
+      {visible && (
+        <>
+          <div className={styles.tabContainer}>
+            <Tabs
+              className={styles.outputTab}
+              defaultActiveKey={'log'}
+              size={'large'}
+              tabPosition={'left'}
+              onChange={handleTabChange}
+              items={items}
+            />
+          </div>
+          {resultSuccess && data.timeCost !== undefined && (
+            <div className={styles.outputFooter} onClick={handleRemoveMenu}>
+              <span>{`${intl.get('console.execTime')} ${data.timeCost / 1000000} (s)`}</span>
+              <div className={styles.btns}>
+                {onExplorer && !!space && (
+                  <Button className="primaryBtn" type="text" onClick={handleExplore}>
+                    {intl.get('common.openInExplore')}
+                  </Button>
+                )}
+              </div>
+            </div>
+          )}
+        </>
       )}
-    </>}
-  </div>;
+    </div>
+  );
 };
 
 export default observer(OutputBox);
