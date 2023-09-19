e02446ff20675e4a1c19f08b6c33e85b66f8c2a9
Nut He <18328704+hetao92@users.noreply.github.com>
2023-09-19 11:58:42 +0800

    mod: remove

--- server/api/studio/pkg/middleware/file.go
+++ server/api/studio/pkg/middleware/file.go
@@ -1,32 +1,15 @@
 package utils
 
 import (
-	"html/template"
 	"io/fs"
 	"net/http"
-	"path/filepath"
 
 	"github.com/vesoft-inc/go-pkg/middleware"
 	"github.com/vesoft-inc/nebula-studio/server/api/studio/internal/svc"
-	"github.com/vesoft-inc/nebula-studio/server/api/studio/pkg/ecode"
-	"github.com/vesoft-inc/nebula-studio/server/api/studio/pkg/utils"
 )
 
 func AssetsMiddlewareWithCtx(svcCtx *svc.ServiceContext, embedAssets fs.FS) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if filepath.Ext(r.URL.Path) == "" {
-			tpl, err := template.ParseFS(embedAssets, "assets/index.html")
-			withErrorMessage := utils.ErrMsgWithLogger(r.Context())
-			if err != nil {
-				svcCtx.ResponseHandler.Handle(w, r, nil, withErrorMessage(ecode.ErrInternalServer, err))
-				return
-			}
-
-			w.Header().Set("Content-Type", "text/html")
-			w.WriteHeader(http.StatusOK)
-			tpl.Execute(w, map[string]any{})
-			return
-		}
 
 		handler := middleware.NewAssetsHandler(middleware.AssetsConfig{
 			Root:       "assets",
