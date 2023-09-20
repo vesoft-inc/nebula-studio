414684a32839fdce8121afdef1266ee90ab829ac
Miao Zhuang <1060950782@163.com>
2023-09-20 14:39:49 +0800

    fix: sync render fiber node (#648)

--- app/pages/SketchModeling/Plugins/SketchShapes/Shapers.tsx
+++ app/pages/SketchModeling/Plugins/SketchShapes/Shapers.tsx
@@ -6,6 +6,7 @@ import { ISchemaEnum } from '@app/interfaces/schema';
 import { NODE_RADIUS } from '@app/config/sketch';
 import Path from './Path';
 import styles from './index.module.less';
+import { flushSync } from 'react-dom';
 export default function initShapes(editor: VEditor) {
   const node = {
     ...DefaultNode.default,
@@ -25,59 +26,60 @@ export default function initShapes(editor: VEditor) {
       // @ts-ignore
       // keep the root, avoid `react createroot multiple times` warning
       node.shape.__reactRoot = root;
-      root.render(
-        <>
-          {!data.hideActive && (
-            <circle className={styles.activeNode} r={NODE_RADIUS + 8} cx={NODE_RADIUS} cy={NODE_RADIUS} />
-          )}
-          <circle
-            className="svg-item"
-            r={NODE_RADIUS - 1}
-            cx={NODE_RADIUS}
-            cy={NODE_RADIUS}
-            strokeDasharray={data.strokeDasharray}
-            style={{
-              strokeWidth: 3,
-              fill: data.fill,
-              stroke: data.strokeColor,
-            }}
-          >
-            <animate
-              attributeName="r"
-              from={24}
-              to={NODE_RADIUS - 1}
-              dur="0.2s"
-              begin="DOMNodeInsertedIntoDocument"
-              fill="freeze"
-            />
-          </circle>
+      flushSync(() => {
+        root.render(
+          <>
+            {!data.hideActive && (
+              <circle className={styles.activeNode} r={NODE_RADIUS + 8} cx={NODE_RADIUS} cy={NODE_RADIUS} />
+            )}
+            <circle
+              className="svg-item"
+              r={NODE_RADIUS - 1}
+              cx={NODE_RADIUS}
+              cy={NODE_RADIUS}
+              strokeDasharray={data.strokeDasharray}
+              style={{
+                strokeWidth: 3,
+                fill: data.fill,
+                stroke: data.strokeColor,
+              }}
+            >
+              <animate
+                attributeName="r"
+                from={24}
+                to={NODE_RADIUS - 1}
+                dur="0.2s"
+                begin="DOMNodeInsertedIntoDocument"
+                fill="freeze"
+              />
+            </circle>
 
-          {data.name ? (
-            <foreignObject x={0} y={0} width={NODE_RADIUS * 2} height={NODE_RADIUS * 2}>
-              <div className={styles.labelContainer}>
-                <span className={styles.label}>{data.name}</span>
-              </div>
-            </foreignObject>
-          ) : null}
-          {data.invalid && (
-            <>
-              <circle cx={NODE_RADIUS * 2 - 10} cy={10} r={12} fill="#EB5757" />
-              <text
-                style={{ userSelect: 'none' }}
-                x={NODE_RADIUS * 2 - 10}
-                y={10}
-                textAnchor="middle"
-                fontSize={16}
-                fill="#fff"
-                dy=".3em"
-              >
-                !
-              </text>
-            </>
-          )}
-        </>,
-      );
-
+            {data.name ? (
+              <foreignObject x={0} y={0} width={NODE_RADIUS * 2} height={NODE_RADIUS * 2}>
+                <div className={styles.labelContainer}>
+                  <span className={styles.label}>{data.name}</span>
+                </div>
+              </foreignObject>
+            ) : null}
+            {data.invalid && (
+              <>
+                <circle cx={NODE_RADIUS * 2 - 10} cy={10} r={12} fill="#EB5757" />
+                <text
+                  style={{ userSelect: 'none' }}
+                  x={NODE_RADIUS * 2 - 10}
+                  y={10}
+                  textAnchor="middle"
+                  fontSize={16}
+                  fill="#fff"
+                  dy=".3em"
+                >
+                  !
+                </text>
+              </>
+            )}
+          </>,
+        );
+      })
       return node.shape;
     },
   };
