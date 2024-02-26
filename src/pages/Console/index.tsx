cd073706c68d9924cabbabadc2acc26b3818a1f4
Bruce <bruce.lu@vesoft.com>
2024-02-26 20:54:18 +0800

    feat: Schema View

--- src/pages/Console/index.tsx
+++ src/pages/Console/index.tsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy, useState } from 'react';
+import { Suspense, lazy, useCallback, useState } from 'react';
 import { useTheme } from '@emotion/react';
 import Box from '@mui/material/Box';
 import MenuList from '@mui/material/MenuList';
@@ -7,7 +7,19 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
 import Select from '@mui/material/Select';
-import { VectorTriangle, FileDocument, Play } from '@vesoft-inc/icons';
+import Collapse from '@mui/material/Collapse';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import SendIcon from '@mui/icons-material/Send';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import Divider from '@mui/material/Divider';
+import { VectorTriangle, FileDocument, Play, ChevronRightFilled } from '@vesoft-inc/icons';
+import styled from '@emotion/styled';
 import {
   ActionWrapper,
   EditorWrapper,
@@ -20,10 +32,18 @@ import {
 
 const MonacoEditor = lazy(() => import('@/components/MonacoEditor'));
 
+const StyledListItemIcon = styled(ListItemIcon)`
+  min-width: auto;
+  margin: ${({ theme }) => theme.spacing(0, 1.5, 0, 1)};
+`;
+
 export default function Console() {
   const theme = useTheme();
   const [activeMenu, setActive] = useState('Console');
   const activeIcon = activeMenu === 'Console' ? <VectorTriangle /> : <FileDocument />;
+  const [open, setOpen] = useState(true);
+
+  const handleClick = useCallback(() => setOpen((open) => !open), []);
   return (
     <Box sx={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
       <StyledSider>
@@ -48,6 +68,57 @@ export default function Console() {
               {activeMenu}
             </Box>
           </SiderItemHeader>
+          <List
+            sx={{ width: '100%' }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+              <ListSubheader
+                component="div"
+                id="nested-list-subheader"
+                sx={{ bgcolor: (theme) => theme.palette.vesoft.bgColor2 }}
+              >
+                NebulaGraph Schema
+              </ListSubheader>
+            }
+          >
+            <Divider />
+            <ListItemButton>
+              <ChevronRightFilled />
+              <StyledListItemIcon>
+                <SendIcon />
+              </StyledListItemIcon>
+              <ListItemText primary="Sent mail" />
+            </ListItemButton>
+            <Divider />
+            <ListItemButton onClick={handleClick}>
+              <ChevronRightFilled
+                sx={{ transform: `rotate(${open ? 90 : 0}deg)`, transition: 'transform ease 0.25s' }}
+              />
+              <StyledListItemIcon>
+                <InboxIcon />
+              </StyledListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Divider />
+              <ListItem sx={{ pl: 6 }}>
+                <ListItemText primary="Starred" />
+              </ListItem>
+              <Divider />
+              <ListItem sx={{ pl: 6 }}>
+                <ListItemText primary="Starred" />
+              </ListItem>
+            </Collapse>
+            <Divider />
+            <ListItemButton>
+              <ChevronRightFilled />
+              <StyledListItemIcon>
+                <DraftsIcon />
+              </StyledListItemIcon>
+              <ListItemText primary="Drafts" />
+            </ListItemButton>
+          </List>
         </SiderItem>
       </StyledSider>
       <Box sx={{ flex: 1 }}>
