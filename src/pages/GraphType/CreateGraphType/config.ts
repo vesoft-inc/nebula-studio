b6d0652995d371b76cd3d08b801cb82481457818
mizy <1060950782@163.com>
2024-03-08 12:07:19 +0800

    feat: creat graph type demo

---
+++ src/pages/GraphType/CreateGraphType/config.ts
@@ -0,0 +1,118 @@
+// @ts-nocheck
+export const NODE_RADIUS = 40;
+
+export const COLOR_LIST = [
+  {
+    fill: '#E6E6E6',
+    strokeColor: 'rgba(60, 60, 60, 0.5)',
+    shadow: 'rgba(90, 90, 90, 0.25)',
+  },
+  {
+    fill: '#E4F1FF',
+    strokeColor: 'rgba(34, 135, 227, 0.5)',
+    shadow: 'rgba(0, 178, 255, 0.25)',
+  },
+  {
+    fill: '#EBE4FF',
+    strokeColor: 'rgba(84, 34, 227, 0.5)',
+    shadow: 'rgba(20, 0, 255, 0.25)',
+  },
+  {
+    fill: '#FEE4FF',
+    strokeColor: 'rgba(227, 34, 196, 0.5)',
+    shadow: 'rgba(255, 0, 229, 0.25)',
+  },
+  {
+    fill: '#FFE4E4',
+    strokeColor: 'rgba(227, 34, 34, 0.5)',
+    shadow: 'rgba(255, 15, 0, 0.25)',
+  },
+  {
+    fill: '#FFF9E4',
+    strokeColor: 'rgba(218, 196, 0, 0.5)',
+    shadow: 'rgba(209, 163, 0, 0.25)',
+  },
+  {
+    fill: '#EFFFE4',
+    strokeColor: 'rgba(54, 200, 2, 0.5)',
+    shadow: 'rgba(0, 255, 10, 0.25)',
+  },
+  {
+    fill: '#E4FFF4',
+    strokeColor: 'rgba(0, 184, 162, 0.5)',
+    shadow: 'rgba(0, 255, 194, 0.25)',
+  },
+];
+
+export const LINE_STYLE = {
+  'stroke-width': 1.6,
+  stroke: 'rgba(99, 111, 129, 0.8)',
+};
+
+export const ARROW_STYLE = {
+  'stroke-width': 1.6,
+  stroke: 'rgba(99, 111, 129, 0.8)',
+  fill: 'transparent',
+  d: 'M7 7L0 0L7 -7',
+  'stroke-linejoin': 'round',
+  'stroke-linecap': 'round',
+};
+
+export function makeLineSort(links) {
+  // update link sort
+  const sourceMap = {};
+  links?.forEach((link) => {
+    const sourceId = link.from;
+    const targetId = link.to;
+    const sourceCommonId = `${sourceId}=>${targetId}`;
+    const targetCommonId = `${targetId}=>${sourceId}`;
+    const linkArr = sourceMap[sourceCommonId] || sourceMap[targetCommonId];
+    if (!linkArr) {
+      sourceMap[sourceCommonId] = [link];
+    } else if (sourceMap[sourceCommonId]) {
+      linkArr.unshift(link);
+    } else if (sourceMap[targetCommonId]) {
+      linkArr.push(link);
+    }
+  });
+  Object.keys(sourceMap).forEach((key) => {
+    if (sourceMap[key].length > 1) {
+      const source = sourceMap[key][0].from;
+      let status = true;
+      let number = sourceMap[key].length % 2 === 0 ? 1 : 0;
+      while (sourceMap[key].length) {
+        const link = status ? sourceMap[key].pop() : sourceMap[key].shift();
+        link.graphIndex = number;
+        // check direction
+        if (link.from !== source) {
+          link.graphIndex *= -1;
+        }
+        number++;
+        status = !status;
+      }
+    } else {
+      const link = sourceMap[key][0];
+      if (link.from === link.to) {
+        link.graphIndex = 1;
+      } else {
+        link.graphIndex = 0;
+      }
+    }
+  });
+}
+
+export function getLinkCurvature(link) {
+  let curvature = 0;
+  const data = link.data;
+  if (data.from === data.to) {
+    curvature = link.graphIndex;
+  } else {
+    const { graphIndex } = data;
+    if (graphIndex !== 0) {
+      const direction = graphIndex % 2 === 0;
+      curvature = (direction ? 1 : -1) * (graphIndex > 0 ? 1 : -1) * (Math.ceil(Math.abs(graphIndex) / 2) * 0.1);
+    }
+  }
+  link.curvature = curvature;
+  return curvature;
+}
--- src/pages/GraphType/CreateGraphType/index.tsx
+++ src/pages/GraphType/CreateGraphType/index.tsx
@@ -1,5 +1,129 @@
-function CreateGraphType() {
-  return <div>CreateGraphType</div>;
-}
+// @ts-nocheck
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useI18n } from '@vesoft-inc/i18n';
+import VEditor from '@vesoft-inc/veditor';
+import { InstanceNode } from '@vesoft-inc/veditor/types/Shape/Node';
+import cls from 'classnames';
+import styles from './index.module.less';
+import initShapes, { initShadowFilter } from './Shapes/Shapers';
+import { NODE_RADIUS, COLOR_LIST } from './config';
 
-export default CreateGraphType;
+const SketchPage: React.FC = () => {
+  const editorRef = useRef<VEditor>();
+  const editorDOMRef = useRef<HTMLDivElement>(null);
+  const { currentLocale } = useI18n();
+  const [draggingNewTag, setDraggingNewTag] = useState<Record<string, unknown>>({});
+  const [draggingPosition, setDraggingPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    editorRef.current = new VEditor({
+      dom: editorDOMRef.current!,
+      showBackGrid: false,
+      disableCopy: true,
+    });
+    initShapes(editorRef.current);
+    initShadowFilter(editorRef.current);
+    editorRef.current.graph.on('node:click', (node: InstanceNode) => {
+      console.log('node:click', node);
+    });
+    return () => {
+      editorRef.current!.destroy();
+    };
+  }, []);
+
+  const onDrag = (e: React.MouseEvent, item) => {
+    e.preventDefault();
+    setDraggingNewTag({
+      ...item,
+      type: 'tag',
+      name: undefined,
+      comment: undefined,
+      properties: [],
+      invalid: false,
+    });
+    setDraggingPosition({
+      x: e.nativeEvent.pageX - 25,
+      y: e.nativeEvent.pageY - 25,
+    });
+    addDragEvents();
+  };
+
+  const addDragEvents = useCallback(() => {
+    const mousemove = (e: MouseEvent) => {
+      if (draggingNewTag) {
+        setDraggingPosition({ x: (e.pageX - 25) as number, y: e.pageY - 25 });
+      }
+    };
+    const mouseup = (e: MouseEvent) => {
+      if (e.target?.tagName === 'svg') {
+        const controller = editorRef.current!.controller;
+        const rect = editorDOMRef.current!.getBoundingClientRect();
+        if (e.clientX - rect.x < 0 || e.clientY - rect.y < 0) {
+          setDraggingNewTag({});
+          return;
+        }
+        const x = (e.clientX - rect.x - controller.x) / controller.scale - 25 * controller.scale;
+        const y = (e.clientY - rect.y - controller.y) / controller.scale - 25 * controller.scale;
+        const node = {
+          x,
+          y,
+          width: NODE_RADIUS * 2,
+          height: NODE_RADIUS * 2,
+          name: undefined,
+          type: 'tag',
+          ...draggingNewTag,
+        };
+        editorRef.current!.graph.node.addNode(node);
+      }
+      setDraggingNewTag({});
+
+      window.document.removeEventListener('mousemove', mousemove);
+      window.document.removeEventListener('mouseup', mouseup);
+    };
+
+    window.document.addEventListener('mousemove', mousemove);
+    window.document.addEventListener('mouseup', mouseup);
+    window.document.addEventListener('mouseleave', mouseup);
+  }, []);
+
+  return (
+    <div className={styles.sketchModeling} key={currentLocale}>
+      <div className={styles.sketchCanvas}>
+        <div id="sketchContainer" className={styles.content} ref={editorDOMRef} />
+        <div className={cls(styles.tagBar, draggingNewTag.uuid && styles.offsetBar)}>
+          <div className={styles.tags}>
+            <span className={styles.tagLabel}>Tag</span>
+            <div className={styles.tagList}>
+              {COLOR_LIST.map((item, index) => (
+                <div
+                  key={index}
+                  style={{
+                    borderColor: item.strokeColor,
+                    backgroundColor: item.fill,
+                    boxShadow: `0 1px 6px ${item.shadow}`,
+                  }}
+                  onMouseDown={(e) => onDrag(e, item)}
+                  className={styles.tagItem}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+        {draggingNewTag.type && (
+          <div
+            className={cls(styles.tagItem, styles.shadowItem)}
+            style={{
+              borderColor: draggingNewTag?.strokeColor,
+              background: draggingNewTag?.fill,
+              display: draggingNewTag ? 'block' : 'none',
+              left: draggingPosition.x,
+              top: draggingPosition.y,
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SketchPage;
